2dbbad0f356739bb4d9ee46596107d7dd5e9a158
hare1901 <hare1901@yandex.ru>
2023-03-19 18:54:09 +0300

    Вот это стоит решить, и апнешь до 5 kuy аккаунт)

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ def alphabet_position(text):
     """
                           Codewars 6 kuy
                           Name : Replace With Alphabet Position
-    In this kata you are required to, given a string, replace every letter with its position in the alphabet.
-    If anything in the text isn't a letter, ignore it and don't return it.
-    "a" = 1, "b" = 2, etc.
     :param text: string
     :return: string
     """
@@ -14,3 +11,59 @@ def alphabet_position(text):
     alp = "abcdefghijklmnopqrstuvwxyz"
     return " ".join([str(alp.find(c) + 1) for c in text.lower() if c in alp])
 
+
+def first_non_repeating_letter(string):
+    """
+                           Codewars: 5 kuy
+                           Name: First non-repeating character
+    :param string: string
+    :return: string
+    """
+    a = [i for i in string if string.lower().count(i.lower()) == 1]
+    if len(a) > 0:
+        return a[0]
+    else:
+        return ''
+
+
+def make_readable(seconds):
+    """
+                           Codewars: 5 kuy
+                           Name: Human Readable Time
+    :param seconds: int
+    :return:
+    """
+    return f'{str(seconds//3600).rjust(2, "0")}:{str(seconds//60%60).rjust(2, "0")}:{str(seconds%60).rjust(2, "0")}'
+
+
+def sum_of_intervals(intervals):
+    """
+                          Codewars 4 kuy
+                          Name: Sum of Intervals
+    :param intervals: [[int]]
+    :return:
+    """
+    if not intervals:
+        return 0
+    sum = 0
+    intervals.sort(key = lambda e: e[0])
+    cursor = intervals[0]
+    for i in range(1, len(intervals)):
+        if intervals[i][0] <= cursor[1]:
+            cursor = (cursor[0], max(cursor[1], intervals[i][1]))
+        else:
+            sum += cursor[1] - cursor[0]
+            cursor = intervals[i]
+    sum += cursor[1] - cursor[0]
+    return sum
+
+
+def move_zeros(lst):
+    """
+                            Codewars 5 kuy
+                            Name: Moving Zeros To The End
+    :param lst: [int]
+    :return: [int]
+    """
+    a = [i for i in lst if i!=0]
+    return a + [0] * (len(lst) - len(a))
